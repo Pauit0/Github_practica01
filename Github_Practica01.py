2ef2e74f0ff36e5bc3693713c2c0e6ceb2fbe1d4
Pauit0 <pau.acosta@escolaindustrial.org>
2022-11-26 11:54:23 +0100

    Create Github_Practica01.py
    Part Meva del codi de clase

---
+++ Github_Practica01.py
@@ -0,0 +1,8 @@
+def triangle(): # 2 Pau A
+    h = float(input("Quan mesura l'altura?"))
+    a = float(input("Quan mesura un costat?"))
+    b = float(input("Quan mesura la base?"))
+    c = float(input("Quan mesura el costat que falta?"))
+    area = b*h/2
+    perimetre = a+b+c
+    return area, perimetre
